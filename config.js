d3f7ee5a60110db8daf0fc0df78b9d81d541ba75
micahdebick <94762872+micahdebick@users.noreply.github.com>
2022-01-27 17:53:18 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,26 @@
+var api_locParams = "lat=36&lon=86";
+// or api_locParams = "q=Paris";
+var api_appId = "6f1c182d1ebe3006634895f631a6f25e";
+var api_lang = "en"
+var api_units = "imperial"; 
+
+var api_params = null;
+
+// refresh time in miliseconds (default 30 mins)
+var refreshTime = 30 * 60 * 1000;
+
+// Browser in kindle paperwhite doesn't support rotation.
+// You can override with this parameter 'll' and 'lr' for landscape left/right, 'up' for upside down.
+//var rotation = "ll";
+
+// You can set night mode,
+// "auto" - by sunrise and sunset,
+// "HH-HH - like: ""22-06", from 22:00 to 06:00
+// "on" - night mode all the day :)
+var nigh_mode = "o"auto
+// Timezone offset - kindle doesnt report correct local time to the kindle (always it is GMT),
+// You can set custom GMT offset, in format "+08:00".
+// You may need to set it again after winter/summer time change.
+// Null is default
+var utcOffset = -6
+
